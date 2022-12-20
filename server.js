3b8941ed3afc65c813d02693bb0f2424f57ce45d
Bao Ngo <b.ngo3048@gmail.com>
2022-12-20 00:26:53 -0500

    added comments

--- server.js
+++ server.js
@@ -16,42 +16,33 @@ const portNumber = process.env.PORT || 5000;
 console.log(`Visit http://localhost:${portNumber}`);
 
 // MongoDB imports 
-const userName = process.env.MONGO_DB_USERNAME;
-const passWord = process.env.MONGO_DB_PASSWORD;
+const username = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
 const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// Connecting to the Mongo Databse
-const uri = `mongodb+srv://${userName}:${passWord}@cluster0.ae9zey0.mongodb.net/?retryWrites=true&w=majority`;
+// Mongo DB credentials
+const uri = `mongodb+srv://${username}:${password}@cluster0.kp5wsfz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function hi(){try {
-	await client.connect();
-} catch(e) {
-	console.log(e);
-}}
+// Start Mongo DB connection
+async function hi(){
+	try {
+		await client.connect();
+	} catch(e) {
+		console.log(e);
+	}}
+
 hi();
 
-// Microsoft Translator Text API
-// Starter code
-const url = 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain';
-
-const options = {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '42e2f88aa4msh9b9e63c519efcd4p1e7f5bjsn71acb1cbaca4',
-    'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-  },
-  body: '[{"Text":"I would really like to drive your car around the block a few times."}]'
-};
-
-// routing section start
+// Routing section start
+// When nothing is specified, display welcome.ejs
 app.get("/", (request, response) => {
-	response.render("welcome", {portNumber:portNumber});
+	response.render("welcome", {portNumber : portNumber});
 });
 
-app.get("/translate", async (request, response)=>{
+// 
+app.get("/translate", async (request, response) => {
 	let currentUser = request.query.username || "";
 	console.log("username insert:" + currentUser);
 
